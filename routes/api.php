abd38cb3377f48cb34f8634dcce79dd2f5784f54
DaniTo <dani.tomicic@ikasle.egibide.org>
2026-01-19 12:25:24 +0100

    feat: Added relationship from backend to visualize students a teacher is tutoring

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TutorCentroController;
 use Illuminate\Support\Facades\Route;
 
+// Login
 Route::post('/login', [LoginController::class, 'login']);
+
+// Obtner allumnos - datos - crear entradas de diario
+Route::get('mis-alumnos/{idTutor}', [TutorCentroController::class, 'getAlumnos']);
+
 
